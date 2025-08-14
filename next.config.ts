a6a4c87ec4599c560767f07b69b769be448a3c44
James <bhdrumacademy@gmail.com>
2025-08-14 13:38:05 +0100

    Remove docker files & update next.config.ts, removed GitHub action

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Produce a minimal standalone server for Docker runtime stage
-  output: "standalone",
 };
 
 export default nextConfig;
